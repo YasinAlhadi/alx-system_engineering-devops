fd9ec52bda3e16783036e499b994d20d5f12cc3e
YasinAlhadi <yasin.a.m.elhadi@gamil.com>
2023-01-11 16:02:25 +0200

    Client configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Client configuration file
+include stdlib
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/sshd_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
