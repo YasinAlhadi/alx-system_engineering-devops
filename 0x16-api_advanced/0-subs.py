d80187a3f30f7349bb95a7bd8ecf0d7e1f0e6b44
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-02-27 19:33:25 +0200

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/pyhton3
-"""a function that queries the Reddit API
+"""
+    a function that queries the Reddit API
     and returns the number of subscribers
 """
 import requests
@@ -7,12 +8,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
-    response = requests.get('https://www.reddit.com/r/{}/about.json'
-                            .format(subreddit),
-                            headers={
-                                        'User-Agent': 'My User Agent 1.0'
-                                        },
-                            allow_redirects=False)
+    redit_url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(redit_url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
